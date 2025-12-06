929d22886aa155e8e7a01a7e69e11072572cd2bd
Haruya Taniguchi <haruya@digitalcube.jp>
2025-12-06 16:16:37 +0900

    feat: Add Astro pages

---
+++ src/lib/microcms.ts
@@ -0,0 +1,28 @@
+import type { MicroCMSQueries, MicroCMSListContent, MicroCMSImage } from "microcms-js-sdk";
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export type Work = {
+  title: string;
+  eyecatch: MicroCMSImage | null;
+  link: string
+} & MicroCMSListContent;
+
+export const getWorks = async (queries?: MicroCMSQueries) => {
+  return await client.getList<Work>({ endpoint: "works", queries });
+};
+
+export const getBlogDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Work>({
+    endpoint: "blogs",
+    contentId,
+    queries,
+  });
+};
